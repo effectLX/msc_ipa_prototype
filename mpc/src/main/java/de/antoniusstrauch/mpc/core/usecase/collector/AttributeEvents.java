bec2abbcdcf4c4edd478ccbec5d0f70d843d92ef
Antonius Strauch <as5021@ic.ac.uk>
2022-08-03 18:20:29 +0100

    attribution logic refined to match most recent attribution

--- mpc/src/main/java/de/antoniusstrauch/mpc/core/usecase/collector/AttributeEvents.java
+++ mpc/src/main/java/de/antoniusstrauch/mpc/core/usecase/collector/AttributeEvents.java
@@ -4,24 +4,40 @@ import de.antoniusstrauch.mpc.core.AUsecase;
 import de.antoniusstrauch.mpc.core.entity.Event;
 import de.antoniusstrauch.mpc.core.entity.EventBatch;
 import de.antoniusstrauch.mpc.core.entity.EventType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedList;
 
 public class AttributeEvents extends AUsecase<EventBatch, Integer> {
 
   @Override
-  public Integer runUsecase(EventBatch batch) {
+  public @NotNull Integer runUsecase(@NotNull EventBatch batch) {
 
+    LinkedList<Event> attributedEvents = new LinkedList<>(); // built-in if attributed events got relevant
     int attribution = 0;
 
+    // Take a trigger event from the batch
     for (Event triggerEvent : batch.getEvents()) {
       if (triggerEvent.getType().equals(EventType.TRIGGER)) {
+      Event recentSourceEvent = null;
+
+        // Iterate through all source events from the batch
         for (Event sourceEvent : batch.getEvents()) {
           if (sourceEvent.getType().equals(EventType.SOURCE)) {
+
+            // Test every source event against the selected trigger event
             if (triggerEvent.getMatchKey().equals(sourceEvent.getMatchKey())
                 && triggerEvent.getTimestamp().isAfter(sourceEvent.getTimestamp())) {
-              attribution++;
-              break;
+              if((recentSourceEvent == null) || (sourceEvent.getTimestamp().isAfter(recentSourceEvent.getTimestamp()))){
+                recentSourceEvent = sourceEvent;
+              }
             }
           }
+        }
+        if (recentSourceEvent != null) {
+          attribution++;
+          attributedEvents.add(triggerEvent);
+          attributedEvents.add(recentSourceEvent);
         }
       }
     }
